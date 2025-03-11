88aab3558256d8cdaff6c2feaa8051c179ff4ece
Dnf4ever <gabriel.f.diaz91@gmail.com>
2025-03-10 20:11:46 -0500

    Create frontEndCall.js
    Example front end call with the example filters in exampleFilterJSON

---
+++ solution1/frontEndCall.js
@@ -0,0 +1,9 @@
+const saveFilters = async (filters) => {
+    const response = await fetch("http://localhost:3000/save-filters", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(filters)
+    });
+    const data = await response.json();
+    return data.shortUrl;  // e.g., "https://app.coolcompany.com/users?filter=abc123xyz"
+};
